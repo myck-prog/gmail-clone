6f0c33f6d857ea314f364ae56706dd879f9f43d3
myck-prog <myckland.learnprog@gmail.com>
2022-09-27 17:56:06 -0700

    Mail Screen Initialize

--- src/EmailList.js
+++ src/EmailList.js
@@ -45,7 +45,7 @@ function EmailList() {
         </div>
       </div>
       <div className="emailList__sections">
-          <Section Icon={InboxIcon} title='primary' color="red" selected />
+          <Section Icon={InboxIcon} title='Primary' color="red" selected />
           <Section Icon={PeopleIcon} title='Social' color="#1A73e8"  />
           <Section Icon={LocalOfferIcon} title='Promotions' color="green"  />
 
